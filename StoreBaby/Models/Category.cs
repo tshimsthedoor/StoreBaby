47cbe8d50e2f27652c483dbb3a04254056acf757
Tresor Tshimanga <tshims79@gmail.com>
2022-04-13 19:17:29 +0200

    Added annotation on Category and Product to specified the names

--- StoreBaby/Models/Category.cs
+++ StoreBaby/Models/Category.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace StoreBaby.Models
 {
     public class Category
     {
         public int ID { get; set; }
+
+        [Display(Name = "Category Name"]
         public string Name { get; set; }
         public virtual ICollection<Product> Products { get; set; }
     }
--- StoreBaby/Models/Product.cs
+++ StoreBaby/Models/Product.cs
@@ -1,8 +1,12 @@
-﻿namespace StoreBaby.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreBaby.Models
 {
     public class Product
     {
         public int ID { get; set; }
+
+        [Display(Name = "Product Name")]
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
