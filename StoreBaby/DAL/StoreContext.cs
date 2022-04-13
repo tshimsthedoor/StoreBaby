86955b4e0a54a357a760241b40fa579bd52dfd18
Tresor Tshimanga <tshims79@gmail.com>
2022-04-13 19:03:10 +0200

    Create baby store entityframework with SQL Database

--- StoreBaby/DAL/StoreContext.cs
+++ StoreBaby/DAL/StoreContext.cs
@@ -3,7 +3,7 @@ using System.Data.Entity;
 
 namespace StoreBaby.DAL
 {
-    public class StoreContext
+    public class StoreContext :DbContext
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
