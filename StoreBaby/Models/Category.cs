42989b00f8fb26203e63cde34b3c6fc849424d34
Tresor Tshimanga <tshims79@gmail.com>
2022-04-13 18:08:43 +0200

    Model
    Added Category model

--- StoreBaby/Models/Category.cs
+++ StoreBaby/Models/Category.cs
@@ -1,6 +1,11 @@
-﻿namespace StoreBaby.Models
+﻿using System.Collections.Generic;
+
+namespace StoreBaby.Models
 {
     public class Category
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
